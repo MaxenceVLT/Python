337a590a87be25635eecad6a86f26cc271cf5db4
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-07-02 00:01:25 +0200

    Mailer v0.1

--- mailer/v0.1/mailer.py
+++ mailer/v0.1/mailer.py
@@ -1,15 +1,12 @@
 import smtplib
 from tkinter import *
-from tkinter import filedialog
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from os.path import basename
 
 popup = Tk()
-popup.title("Email")
+popup.title("Mailer")
 popup.geometry("520x140")
-#popup.resizable(0,0)
+popup.resizable(0,0)
 
 label1=Label(popup,text="Email de l'expediteur")
 label1.grid(row=0, column=0)
@@ -26,7 +23,6 @@ label2.grid(row=2, column=0)
 receiver_email_entry=Entry(popup,width=25)
 receiver_email_entry.grid(row=2, column=1)
 
-
 label3=Label(popup,text="Sujet")
 label3.grid(row=3, column=0)
 subject_message_entry=Entry(popup,width=25)
@@ -37,16 +33,6 @@ label4.grid(row=4, column=0)
 message_entry=Entry(popup,width=25)
 message_entry.grid(row=4, column=1)
 
-file_label = Label(popup,text="test")
-file_label.grid(row=4, column=3)
-
-def get_file():
-    file_test =  filedialog.askopenfilename()
-    file_label.config(text=file_test)
-
-button_attachement=Button(popup,text="Joindre",width=7,command=get_file)
-button_attachement.grid(row=4, column=2)
-
 def send():
     gmail = smtplib.SMTP('smtp.gmail.com', 587) 
     gmail.starttls() 
@@ -54,17 +40,9 @@ def send():
     msg = MIMEMultipart()
     msg['From'] = sender_email_entry.get()
     msg['To'] = receiver_email_entry.get()
-    msg['Subject'] = subject_message_entry.get()  
+    msg['Subject'] = subject_message_entry.get()
     message = message_entry.get()
     msg.attach(MIMEText(message))
-
-    fileName = file_label.cget("text")
-    file = open(fileName, "rb")
-    fileBaseName = basename(fileName)
-    part = MIMEApplication(file.read(), Name = fileBaseName)
-    part.add_header('Content-Disposition', 'attachment; filename="' + fileBaseName + '"')
-    msg.attach(part)
-
     gmail.send_message(msg)
     gmail.quit()
     label4=Label(popup,text="Message correctement envoyé")
